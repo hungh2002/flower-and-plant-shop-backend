abb8cf898e922ab47ee742c835d9d2a5513fef8a
hungh2002 <hungg562002@gmail.com>
2024-08-05 18:48:45 +0900

    Setup mariaDB connection

---
+++ src/main/java/com/hungh2002/config/DBConnection.java
@@ -0,0 +1,35 @@
+package com.hungh2002.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * DBConnection
+ */
+public class DBConnection {
+
+    protected Connection connection = null;
+
+    public DBConnection() {
+        String databaseUrl = "jdbc:" + GlobalVariable.RDBMS + "://" + GlobalVariable.HOST + ":"
+                + GlobalVariable.PORT_OF_HOST + "/" + GlobalVariable.DATABASE_NAME;
+        try {
+            connection = DriverManager.getConnection(databaseUrl, GlobalVariable.USERNAME,
+                    GlobalVariable.PASSWORD);
+        } catch (Exception e) {
+            System.out.println("Database connection error: " + e);
+        }
+
+    }
+
+    public void close() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            System.out.println("ERROR: DBConnection -> Close: " + e);
+        }
+    }
+
+}
---
+++ src/main/java/com/hungh2002/config/GlobalVariable.java
@@ -0,0 +1,21 @@
+package com.hungh2002.config;
+
+/**
+ * GlobalVariable
+ */
+public class GlobalVariable {
+
+    // Database configuration
+    //// relational database management system
+    public static final String RDBMS = "mariadb";
+    public static final String HOST = "localhost";
+    public static final int PORT_OF_HOST = 3306;
+    //////
+    /////// database name
+    public static final String DATABASE_NAME = "flower-and-plant-shop";
+    //////// database name --> used for testing
+    // public static final String DATABASE_NAME = "flower-and-plant-shop-backend_test";
+    //////
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "1905";
+}
